ada449df2fa44b4b7ab05f3c33997a36db08f0b0
sherlock_lmy <1243935366@qq.com>
2025-02-26 21:48:31 +0800

    __restrict__

---
+++ src/chapter09/08_restrict.c
@@ -0,0 +1,13 @@
+//
+// Created by Shorlock on 2025/2/16.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    // __restrict__ 添加了该关键字表示存不会被覆盖
+    return 0;
+}
